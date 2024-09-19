ecfbcd5badffc579916eeae4bff8d49954295e63
adithyasean <adithyasean@outlook.com>
2024-09-20 01:46:28 +0530

    Refactor transliterate_to_huggingface.py script

---
+++ scripts/push_to_huggingface.py
@@ -0,0 +1,18 @@
+from transliterate import sinhala_to_singlish
+from datasets import load_dataset, DatasetDict
+
+def push_to_huggingface():
+    input_dataset_repository = input("Enter the Hugging Face dataset repository name: ")
+    output_dataset_repository = input("Enter the Hugging Face dataset repository name to push the transliterated dataset: ")
+    api_token = input("Enter your Hugging Face API token: ")
+
+    dataset = load_dataset(input_dataset_repository)
+
+    transliterated_splits = {}
+    for split in dataset.keys():
+        transliterated_splits[split] = dataset[split].map(lambda example: {"text": sinhala_to_singlish(example["text"])})
+
+    transliterated_dataset = DatasetDict(transliterated_splits)
+    transliterated_dataset.push_to_hub(output_dataset_repository, token=api_token)
+
+push_to_huggingface()

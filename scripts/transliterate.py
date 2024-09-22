bd8640402f78b44a531ed1a020906f9966559660
AdithyaSean <adithyasean@gmail.com>
2024-09-22 15:01:51 +0530

    Refactor push_to_huggingface.py script and remove unnecessary input prompt for Hugging Face API token

--- scripts/transliterate.py
+++ scripts/transliterate.py
@@ -4,7 +4,6 @@ from datasets import load_dataset, DatasetDict
 def push_to_huggingface():
     input_dataset_repository = input("Enter the Hugging Face dataset repository name: ")
     output_dataset_repository = input("Enter the Hugging Face dataset repository name to push the transliterated dataset: ")
-    api_token = input("Enter your Hugging Face API token: ")
 
     dataset = load_dataset(input_dataset_repository)
 
@@ -13,6 +12,6 @@ def push_to_huggingface():
         transliterated_splits[split] = dataset[split].map(lambda example: {"text": sinhala_to_singlish(example["text"])})
 
     transliterated_dataset = DatasetDict(transliterated_splits)
-    transliterated_dataset.push_to_hub(output_dataset_repository, token=api_token, private=True)
+    transliterated_dataset.push_to_hub(output_dataset_repository, private=True)
 
 push_to_huggingface()
